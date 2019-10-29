1c92e27623c22e23e146f99ec55e768bd911042b
Tony Lomax <antl.lomax@gmail.com>
2019-10-29 19:47:30 +0000

    Initial react app set up

---
+++ pay_me/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <section>
+      <h1> PayMe </h1>
+    </section>
+  );
+}
+
+export default App;
