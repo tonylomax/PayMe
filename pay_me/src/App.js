70442e734892eb0913c745feed3e9eae5049fb43
Tony Lomax <antl.lomax@gmail.com>
2019-10-29 20:00:59 +0000

    Getting ready for swift

--- pay_me/src/App.js
+++ pay_me/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
     <section>
       <h1> PayMe </h1>
+      <LandingPage></LandingPage>
     </section>
   );
 }
---
+++ pay_me/src/pages/MainPage.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MainPage() {
+  return <h1> This is where stripe will go</h1>;
+}
