0f6f1dc88b5999c015465176b4b5c6e38a4ca0a7
Tony Lomax <antl.lomax@gmail.com>
2019-11-05 13:56:28 +0000

    Checkout form test

--- pay_me/src/components/CheckoutFrom.jsx
+++ pay_me/src/components/CheckoutFrom.jsx
@@ -1,31 +1,32 @@
 import React, { Component } from "react";
 import { CardElement, injectStripe } from "react-stripe-elements";
 
-// class CheckoutForm extends Component
+class CheckoutForm extends Component {
+  constructor(props) {
+    super(props);
+    this.submit = this.submit.bind(this);
+  }
 
-function CheckoutForm({ injectStripe }) {
-  const { complete, setComplete } = React.useState(false);
-
-  const submit = async ev => {
-    let { token } = await injectStripe.stripe.createToken({
-      name: "Name"
-    });
+  async submit(ev) {
+    let { token } = await this.props.stripe.createToken({ name: "Name" });
     let response = await fetch("/charge", {
       method: "POST",
       headers: { "Content-Type": "text/plain" },
       body: token.id
     });
 
-    if (response.ok) console.log("Purchase Complete!" && setComplete(true));
-  };
+    if (response.ok) console.log("Purchase Complete!", response);
+  }
 
-  return (
-    <div className="checkout">
-      <p>Would you like to complete the purchase?</p>
-      <CardElement />
-      <button onClick={submit}>Purchase</button>
-    </div>
-  );
+  render() {
+    return (
+      <div className="checkout">
+        <p>Would you like to complete the purchase?</p>
+        <CardElement />
+        <button onClick={this.submit}>Purchase</button>
+      </div>
+    );
+  }
 }
 
 export default injectStripe(CheckoutForm);
